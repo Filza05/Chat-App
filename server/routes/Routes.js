bc1387e9c51c573ad6bfff5e99f9e9d527770a68
Filza Khan Tareen <filzatareen@gmail.com>
2023-08-15 17:34:53 +0500

    Added email verification at signup

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/userModel");
+const sendVerificationCode = require("../Utils/emailVerificationCode");
 
 const Router = express.Router();
 
 Router.post("/sign-up", async (req, res) => {
   const user = req.body;
-  user.password = await bcrypt.hash(user.password, 12);
-  const newUser = new UserModel(user);
+  const email = user.email;
+  const existingUser = await UserModel.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ error: "Email already registered" });
+  }
 
   try {
-    const userDoc = await newUser.save();
-    console.log("user saved to database");
-    res.status(201).json(userDoc);
+    await sendVerificationCode(user.email);
+    res
+      .status(201)
+      .json({ message: "verification code sent succesfully" });
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    console.log(error);
   }
+
+  // const newUser = new UserModel(user);
+  // user.password = await bcrypt.hash(user.password, 12);
+
+  // try {
+  //   const userDoc = await newUser.save();
+  //   console.log("user saved to database");
+  //   res.status(201).json(userDoc);
+  // } catch (error) {
+  //   res.status(500).json("internal server error");
+  // }
 });
 
 module.exports = { Router };
